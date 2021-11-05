b2722bde3920bbc747263d297e15e4e602a34ba6
Alexander Korniichuk <world.kor.films@gmail.com>
2021-11-05 22:06:24 +0200

    TypeScript OOP (interface and class inheritance)

--- src/ts/app.js
+++ src/ts/app.js
@@ -1,152 +1,40 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("--- Typescript File ---");
-var person = { id: 12, name: "Alex", surname: "Kor", age: 12 };
-console.log(person);
-// const myFun = (a: number, b: number): number => a + b
-// const myFun = (a: number, b: number): number => {
-//     return a + b
-// }
-// console.log(myFun(2, 5));
-var myObject = { id: 12, name: "Alex", surname: "Kor", age: 12, phone: "586-7656" };
-var printNameOrDescription = function (value) { return console.log('phone' in value); };
-printNameOrDescription(myObject);
-var SimpleClass = /** @class */ (function () {
-    function SimpleClass() {
+var InterfaceInheritanceClass = /** @class */ (function () {
+    function InterfaceInheritanceClass() {
     }
-    SimpleClass.prototype.print = function () {
-        console.log("SimpleClass has id : " + this.id);
-    };
-    return SimpleClass;
+    return InterfaceInheritanceClass;
 }());
-var mySimple = new SimpleClass();
-mySimple.id = 123;
-mySimple.print();
-console.clear();
-var ClassWithConstructor = /** @class */ (function () {
-    function ClassWithConstructor(_id, _name) {
-        this.id = _id;
-        this.name = _name;
+/////////////////////////////////////////////////////////
+var BaseClass = /** @class */ (function () {
+    function BaseClass() {
     }
-    return ClassWithConstructor;
+    return BaseClass;
 }());
-var classWithConstructor = new ClassWithConstructor(1, "name");
-console.log("classWithConstructor = " + JSON.stringify(classWithConstructor, undefined, 2));
-console.clear();
-var ComplexType = /** @class */ (function () {
-    function ComplexType(idArg, nameArg) {
-        if (typeof idArg === "number")
-            this.id = idArg;
-        this.name = nameArg;
+var DerivedFromBaseClass = /** @class */ (function (_super) {
+    __extends(DerivedFromBaseClass, _super);
+    function DerivedFromBaseClass() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    ComplexType.prototype.print = function () {
-        return "id:" + this.id + " name:" + this.name;
-    };
-    ComplexType.prototype.usingTheAnyKeyword = function (arg1) {
-        this.id = arg1;
-    };
-    ComplexType.prototype.usingOptionalParameters = function (optionalArg1) {
-        if (optionalArg1) {
-            this.id = optionalArg1;
-        }
-    };
-    ComplexType.prototype.usingDefaultParameters = function (defaultArg1) {
-        if (defaultArg1 === void 0) { defaultArg1 = 0; }
-        this.id = defaultArg1;
-    };
-    ComplexType.prototype.usingRestSyntax = function () {
-        var argArray = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            argArray[_i] = arguments[_i];
-        }
-        if (argArray.length > 0) {
-            this.id = argArray[0];
-        }
-    };
-    ComplexType.prototype.usingFunctionCallbacks = function (callback) {
-        callback(this.id);
-    };
-    return ComplexType;
-}());
-var ct_1 = new ComplexType(1, "ct_1");
-var ct_2 = new ComplexType("abc", "ct_2");
-// console.log(`= ${JSON.stringify(ct_2, undefined, 2)}`);
-console.log(ct_2.print());
-ct_1.usingTheAnyKeyword(true);
-ct_1.usingTheAnyKeyword({ id: 1, name: "string" });
-// console.log(` = ${JSON.stringify(ct_1, undefined, 2)}`);
-ct_1.usingOptionalParameters(1);
-ct_1.usingOptionalParameters();
-// console.log(` = ${JSON.stringify(ct_1, undefined, 2)}`);
-ct_1.usingRestSyntax(1, 2, 3);
-ct_2.usingRestSyntax(1, 2, 3, 4, 5);
-console.log("ct_1 = " + JSON.stringify(ct_1, undefined, 2));
-console.log("ct_2 = " + JSON.stringify(ct_2, undefined, 2));
-var myCallbackFunction = function (id) { return id.toString(); };
-ct_1.usingFunctionCallbacks(myCallbackFunction);
-console.log(" = " + JSON.stringify(ct_1, undefined, 2));
-console.clear();
-///////////////////////////////////////
-var ClassWithPublicProperty = /** @class */ (function () {
-    function ClassWithPublicProperty() {
+    return DerivedFromBaseClass;
+}(BaseClass));
+var MultipleInterfaces = /** @class */ (function () {
+    function MultipleInterfaces() {
     }
-    return ClassWithPublicProperty;
+    return MultipleInterfaces;
 }());
-var publicAccess = new ClassWithPublicProperty();
-publicAccess.id = 10;
-// console.log(`publicAccess = ${JSON.stringify(publicAccess, undefined, 2)}`);
-var classWithAutomaticProperties = /** @class */ (function () {
-    function classWithAutomaticProperties(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-    return classWithAutomaticProperties;
-}());
-var myAutoClass = new classWithAutomaticProperties(1, "className");
-// console.log(`myAutoClass = ${JSON.stringify(myAutoClass, undefined, 2)}`);
-var StaticProperty = /** @class */ (function () {
-    function StaticProperty() {
-        this.name = "Mike";
-    }
-    StaticProperty.prototype.updateCount = function () {
-        StaticProperty.count++;
-    };
-    StaticProperty.printToConsole = function () {
-        console.log("Hello console from print method");
-    };
-    StaticProperty.count = 0;
-    return StaticProperty;
-}());
-var obj = new StaticProperty();
-// console.log(`obj = ${JSON.stringify(obj, undefined, 2)}`);
-obj.updateCount();
-////////////// Namespaces /////////
-var FirstNameSpace;
-(function (FirstNameSpace) {
-    var NotExported = /** @class */ (function () {
-        function NotExported() {
-        }
-        return NotExported;
-    }());
-    var NameSpaceClass = /** @class */ (function () {
-        function NameSpaceClass(id) {
-            this.id = id;
-        }
-        return NameSpaceClass;
-    }());
-    FirstNameSpace.NameSpaceClass = NameSpaceClass;
-    var something = new NotExported(); // possible only here because of absence of export in class defining
-})(FirstNameSpace || (FirstNameSpace = {}));
-var firstNameSpace = new FirstNameSpace.NameSpaceClass(1234);
-console.log("firstNameSpace = " + JSON.stringify(firstNameSpace, undefined, 2));
-var SecondNameSpace;
-(function (SecondNameSpace) {
-    var NameSpaceClass = /** @class */ (function () {
-        function NameSpaceClass(name) {
-            this.name = name;
-        }
-        return NameSpaceClass;
-    }());
-    SecondNameSpace.NameSpaceClass = NameSpaceClass;
-})(SecondNameSpace || (SecondNameSpace = {}));
-var secondNameSpace = new SecondNameSpace.NameSpaceClass("Alex");
-console.log("secondNameSpace = " + JSON.stringify(secondNameSpace, undefined, 2));
 //# sourceMappingURL=app.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,212 +1,39 @@
 console.log("--- Typescript File ---")
 
-interface MyInterface {
-    id: number
-    name: string
-    surname: string
-    age: number
-}
+///////// interface inheritance /////////
 
-interface ImySecond {
-    phone: string
-    code: number
-}
-
-
-let person: MyInterface = {id: 12, name: "Alex", surname: "Kor", age: 12}
-console.log(person);
-
-// const myFun = (a: number, b: number): number => a + b
-// const myFun = (a: number, b: number): number => {
-//     return a + b
-// }
-
-// console.log(myFun(2, 5));
-
-const myObject: MyInterface | ImySecond = {id: 12, name: "Alex", surname: "Kor", age: 12, phone: "586-7656"}
-
-const printNameOrDescription = (value: MyInterface | ImySecond): void => console.log('phone' in value)
-printNameOrDescription(myObject)
-
-
-class SimpleClass {
+interface IBase {
     id: number | undefined;
-
-    print(): void {
-        console.log(`SimpleClass has id : ${this.id}`);
-    }
 }
 
-const mySimple = new SimpleClass()
-mySimple.id = 123
-mySimple.print()
-console.clear()
-
-
-class ClassWithConstructor {
-    id: number;
-    name: string;
-
-    constructor(_id: number, _name: string) {
-        this.id = _id;
-        this.name = _name;
-    }
+interface IDerivedFromBase extends IBase {
+    name: string | undefined;
 }
 
-let classWithConstructor = new ClassWithConstructor(1, "name");
-console.log(`classWithConstructor = ${JSON.stringify(classWithConstructor, undefined, 2)}`);
-console.clear()
+class InterfaceInheritanceClass implements IDerivedFromBase {
+    id: number | undefined;
+    name: string | undefined;
+}
+/////////////////////////////////////////////////////////
 
-///////////////////////////////////////
-interface IComplexType {
-    id: number;
-    name: string;
-
-    print(): string;
-
-    usingTheAnyKeyword(arg1: any): any;
-
-    usingOptionalParameters(optionalArg1?: number): void;
-
-    usingDefaultParameters(defaultArg1?: number): void;
-
-    usingRestSyntax(...argArray: number []): void;
-
-    usingFunctionCallbacks(callback: (id: number) => string);
+class BaseClass implements IBase {
+    id: number | undefined;
 }
 
+class DerivedFromBaseClass extends BaseClass implements IDerivedFromBase {
+    name: string | undefined;
+}
+//////////////////////////////////////////////////////////
 
-class ComplexType implements IComplexType {
-    id: number;
-    name: string;
-
-    constructor(idArg: number, nameArg: string);
-    constructor(idArg: string, nameArg: string);
-    constructor(idArg: any, nameArg: any) {
-        if (typeof idArg === "number")
-            this.id = idArg;
-
-        this.name = nameArg;
-    }
-
-    print(): string {
-        return "id:" + this.id + " name:" + this.name;
-    }
-
-    usingTheAnyKeyword(arg1: any): any {
-        this.id = arg1;
-    }
-
-    usingOptionalParameters(optionalArg1?: number) {
-        if (optionalArg1) {
-            this.id = optionalArg1;
-        }
-    }
-
-    usingDefaultParameters(defaultArg1: number = 0) {
-        this.id = defaultArg1;
-    }
-
-    usingRestSyntax(...argArray: number []) {
-        if (argArray.length > 0) {
-            this.id = argArray[0];
-        }
-    }
-
-    usingFunctionCallbacks(callback: (id: number) => string) {
-        callback(this.id);
-    }
+interface IFirstInterface {
+    id: number | undefined;
 }
 
-let ct_1 = new ComplexType(1, "ct_1");
-let ct_2 = new ComplexType("abc", "ct_2");
-
-// console.log(`= ${JSON.stringify(ct_2, undefined, 2)}`);
-console.log(ct_2.print());
-
-ct_1.usingTheAnyKeyword(true);
-ct_1.usingTheAnyKeyword({id: 1, name: "string"});
-// console.log(` = ${JSON.stringify(ct_1, undefined, 2)}`);
-
-ct_1.usingOptionalParameters(1);
-ct_1.usingOptionalParameters();
-// console.log(` = ${JSON.stringify(ct_1, undefined, 2)}`);
-
-ct_1.usingRestSyntax(1, 2, 3);
-ct_2.usingRestSyntax(1, 2, 3, 4, 5);
-console.log(`ct_1 = ${JSON.stringify(ct_1, undefined, 2)}`);
-console.log(`ct_2 = ${JSON.stringify(ct_2, undefined, 2)}`);
-
-const myCallbackFunction = (id: number): string => id.toString()
-ct_1.usingFunctionCallbacks(myCallbackFunction);
-console.log(` = ${JSON.stringify(ct_1, undefined, 2)}`);
-console.clear()
-
-///////////////////////////////////////
-
-class ClassWithPublicProperty {
-    public id: number | undefined;
+interface ISecondInterface {
+    name: string | undefined;
 }
 
-let publicAccess = new ClassWithPublicProperty();
-publicAccess.id = 10;
-
-// console.log(`publicAccess = ${JSON.stringify(publicAccess, undefined, 2)}`);
-
-class classWithAutomaticProperties {
-    constructor(
-        public id: number,
-        private name: string) {
-    }
+class MultipleInterfaces  implements IFirstInterface, ISecondInterface {
+    id: number | undefined;
+    name: string | undefined;
 }
-
-let myAutoClass = new classWithAutomaticProperties(1, "className");
-
-// console.log(`myAutoClass = ${JSON.stringify(myAutoClass, undefined, 2)}`);
-
-class StaticProperty {
-    static count = 0;
-    public name: string = "Mike"
-
-    updateCount() {
-        StaticProperty.count++;
-    }
-
-    static printToConsole(): void {
-        console.log("Hello console from print method")
-    }
-}
-
-const obj = new StaticProperty()
-// console.log(`obj = ${JSON.stringify(obj, undefined, 2)}`);
-obj.updateCount()
-
-
-////////////// Namespaces /////////
-namespace FirstNameSpace {
-    class NotExported {
-    }
-
-    export class NameSpaceClass {
-        constructor(
-            public id: number | undefined) {
-        }
-    }
-
-    const something = new NotExported() // possible only here because of absence of export in class defining
-}
-
-let firstNameSpace = new FirstNameSpace.NameSpaceClass(1234);
-console.log(`firstNameSpace = ${JSON.stringify(firstNameSpace, undefined, 2)}`);
-
-
-namespace SecondNameSpace {
-    export class NameSpaceClass {
-        constructor(
-            public name: string | undefined) {
-        }
-    }
-}
-
-const secondNameSpace = new SecondNameSpace.NameSpaceClass("Alex")
-console.log(`secondNameSpace = ${JSON.stringify(secondNameSpace, undefined, 2)}`);
